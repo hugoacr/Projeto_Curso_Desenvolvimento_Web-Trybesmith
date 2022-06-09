19cd8ae40851799215f1f4163ec3c35e079a5605
Hugoacr <hugoacr@yahoo.com.br>
2022-06-09 18:01:03 -0300

    adciona rota de produtos no app.

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import UsersRoutes from './routes/usersRoutes';
+import ProductsRoutes from './routes/productsRoutes';
+import 'express-async-errors';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(UsersRoutes);
+
+app.use(ProductsRoutes);
 
 export default app;
