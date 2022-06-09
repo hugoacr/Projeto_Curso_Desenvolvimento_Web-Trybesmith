c1397fff00e3de448632cec637792d74a7c60632
Hugoacr <hugoacr@yahoo.com.br>
2022-06-09 15:25:30 -0300

    cria rota do user e adciona ao app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import UsersRoutes from './routes/usersRoutes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(UsersRoutes);
 
 export default app;
