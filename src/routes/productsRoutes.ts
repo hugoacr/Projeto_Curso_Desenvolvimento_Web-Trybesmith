8bc2b8174d3fee2bde85ec942f43925187822d55
Hugoacr <hugoacr@yahoo.com.br>
2022-06-09 19:24:32 -0300

    cria rota para adição de produto

--- src/routes/productsRoutes.ts
+++ src/routes/productsRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import ProductsController from '../controllers/productsController';
+import validationProduct from '../middlewares/productsMiddleware';
 
 const router = Router();
 
 const productsController = new ProductsController();
 
 router.get('/products', productsController.getAll);
+router.post('/products', validationProduct, productsController.create);
 
 export default router;
