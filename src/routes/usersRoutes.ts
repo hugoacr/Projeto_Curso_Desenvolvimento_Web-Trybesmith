998907f96efe8bc5326d28b4d9969656de1aecca
Hugoacr <hugoacr@yahoo.com.br>
2022-06-14 12:55:57 -0300

    cria rota para endpoint POST /users para novo user

--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -1,5 +1,11 @@
 import { Router } from 'express';
+import UsersController from '../controllers/usersController';
+import UsersValidation from '../middlewares/usersMiddleware';
 
 const router = Router();
+
+const usersController = new UsersController();
+
+router.post('/users', UsersValidation, usersController.create);
 
 export default router;
