c388ee0afa061996ba77c633246df478c62c81c0
Hugoacr <hugoacr@yahoo.com.br>
2022-06-18 13:27:33 -0300

    cria models para EndPoint Get /orders

---
+++ src/models/ordersModels.ts
@@ -0,0 +1,19 @@
+import { Pool } from 'mysql2/promise';
+import IOrder from '../interfaces/orderInterface';
+
+export default class OrderModel {
+  public connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public async getAll(): Promise<IOrder[]> {
+    const result = await this.connection
+      .execute(`
+        SELECT O.id, O.userId, P.id as productsIds FROM Trybesmith.Orders AS O 
+        INNER JOIN Trybesmith.Products as P ON P.orderId = O.id`);
+    const [rows] = result;
+    return rows as IOrder[];
+  }
+}
