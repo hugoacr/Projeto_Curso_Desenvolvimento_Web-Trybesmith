237ae9891a458e0ee0a7919f91d3af4d5013feb8
Hugoacr <hugoacr@yahoo.com.br>
2022-06-18 13:28:11 -0300

    cria service para EndPoint Get /orders

---
+++ src/services/ordersService.ts
@@ -0,0 +1,18 @@
+import connection from '../models/connection';
+import IOrder from '../interfaces/orderInterface';
+import OrderModel from '../models/ordersModels';
+
+class ProductService {
+  public model: OrderModel;
+
+  constructor() {
+    this.model = new OrderModel(connection);
+  }
+
+  public async getAll(): Promise<IOrder[]> {
+    const products = await this.model.getAll();
+    return products;
+  }
+}
+
+export default ProductService;
